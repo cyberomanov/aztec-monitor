cd9cbb7251b13493d5c5f21491bd8b706dd3dea2
cyberomanov <romanoff_1@bk.ru>
2025-08-13 12:57:46 +0300

    v1.2 - report

--- user_data/config-example.py
+++ user_data/config-example.py
@@ -1,7 +1,16 @@
+# максимальное количество попыток при ошибках
 max_retries = 3
+
+# мобильный прокси для взаимодействия с aztecscan и dashtec
 mobile_proxy = "socks5://log:pass@ip:port"
+# mobile_proxy = "http://log:pass@ip:port"
+
+# сон в секундах между аккаунтами
 sleep_between_accs = (3, 5)
+# сон в секундах между кругами
 sleep_between_loop = (600, 800)
 
-bot_api_key = "20xxx9512:Axxxxk"
+# апи ключ от телеграм бота
+bot_api_key = "22222:AAA-BBB"
+# id чата, куда слать уведомления при критических показателях
 alarm_chat_id = "-1111"
