f0637d3789905e4760257fdc0368963e086f2432
cyberomanov <romanoff_1@bk.ru>
2025-08-14 13:28:34 +0300

    v1.3 - minor

--- user_data/config.py
+++ user_data/config.py
@@ -1,2 +1,2 @@
-# перенесите и настройте параметры из примера: 'user_data/config-example.py'.
-# не изменяйте файл 'user_data/config-example.py' во избежание проблем с обновлениями версий софта.
+# copy and configure parameters from example: 'user_data/config-example.py'.
+# do not modify 'user_data/config-example.py' file to avoid issues with software version updates.
